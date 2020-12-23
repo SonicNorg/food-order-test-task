a1ddb9fc5c195e218488605b22f5f9db918277f9
Krizhanovskiy Pavel <pavel.krizhanovskiy@tieto.com>
2020-12-23 16:42:37 +0300

    WIP: Restaurant startup broken

--- resta-service/src/main/java/name/nepavel/foodorder/restaservice/controller/RestaController.java
+++ resta-service/src/main/java/name/nepavel/foodorder/restaservice/controller/RestaController.java
@@ -1,7 +1,9 @@
 package name.nepavel.foodorder.restaservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import name.nepavel.foodorder.restaapi.model.FoodItem;
 import name.nepavel.foodorder.restaapi.model.OrderItem;
+import name.nepavel.foodorder.restaservice.service.RestaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import name.nepavel.foodorder.restaapi.api.RestaurantApi;
@@ -10,11 +12,13 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class RestaController implements RestaurantApi {
+    private final RestaService restaService;
 
     @Override
     public ResponseEntity<List<FoodItem>> menuGet() {
-        return null;
+        return ResponseEntity.ok(restaService.list());
     }
 
     @Override
