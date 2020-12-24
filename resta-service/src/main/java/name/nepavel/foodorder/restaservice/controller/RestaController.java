9cd585fb87c56d8301b0d1c187eb4d54a89f1edd
Krizhanovskiy Pavel <pavel.krizhanovskiy@tieto.com>
2020-12-24 14:41:48 +0300

    Get orders

--- resta-service/src/main/java/name/nepavel/foodorder/restaservice/controller/RestaController.java
+++ resta-service/src/main/java/name/nepavel/foodorder/restaservice/controller/RestaController.java
@@ -1,13 +1,13 @@
 package name.nepavel.foodorder.restaservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import name.nepavel.foodorder.restaapi.api.RestaurantApi;
 import name.nepavel.foodorder.restaapi.model.FoodItem;
-import name.nepavel.foodorder.restaapi.model.Order;
 import name.nepavel.foodorder.restaapi.model.OrderItem;
+import name.nepavel.foodorder.restaapi.model.OrderResponse;
 import name.nepavel.foodorder.restaservice.service.RestaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import name.nepavel.foodorder.restaapi.api.RestaurantApi;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,12 +18,22 @@ public class RestaController implements RestaurantApi {
     private final RestaService restaService;
 
     @Override
-    public ResponseEntity<List<FoodItem>> menuGet() {
+    public ResponseEntity<List<FoodItem>> getMenu() {
         return ResponseEntity.ok(restaService.list());
     }
 
     @Override
-    public ResponseEntity<Order> orderPost(@Valid List<OrderItem> orderItem) {
+    public ResponseEntity<List<OrderResponse>> getOrders() {
+        return ResponseEntity.ok(restaService.getOrders());
+    }
+
+    @Override
+    public ResponseEntity<OrderResponse> getOrderById(Integer id) {
+        return ResponseEntity.ok(restaService.getOrder(id));
+    }
+
+    @Override
+    public ResponseEntity<OrderResponse> createOrder(@Valid List<OrderItem> orderItem) {
         return ResponseEntity.ok(restaService.order(orderItem));
     }
 
